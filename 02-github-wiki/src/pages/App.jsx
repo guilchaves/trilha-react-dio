c28ff5324a130bf3b9771ec03b1a1d1430a0aac1
guilherme <guilgchaves@protonmail.com>
2023-03-16 00:55:10 -0300

    add try catch block and add remove functionality

--- 02-github-wiki/src/pages/App.jsx
+++ 02-github-wiki/src/pages/App.jsx
@@ -11,24 +11,28 @@ const App = () => {
   const [repos, setRepos] = useState([]);
 
   const handleSearchRepo = async () => {
-    const { data } = await api.get(`repos/${currentRepo}`);
+    try {
+      const { data } = await api.get(`repos/${currentRepo}`);
 
-    if (data.id) {
-      const alreadyFound = repos.find(repo => repos.id === data.id);
+      if (data.id) {
+        const alreadyFound = repos.find(repo => repos.id === data.id);
 
-      if (!alreadyFound) {
-        setRepos(prev => [...prev, data]);
-        setCurrentRepo('');
-        return;
+        if (!alreadyFound) {
+          setRepos(prev => [...prev, data]);
+          setCurrentRepo('');
+          return;
+        }
       }
+    } catch (err) {
+      console.log(err);
     }
 
     alert('Repositório não encontrado');
   };
 
-  //todo
   const handleRemoveRepo = id => {
-    console.log('Removendo registo', id);
+    const filteredId = repos.filter(repo => id !== repo.id);
+    setRepos(filteredId);
   };
 
   return (
@@ -37,7 +41,7 @@ const App = () => {
       <Input value={currentRepo} onChange={e => setCurrentRepo(e.target.value)} />
       <Button onClick={handleSearchRepo} />
       {repos.map(repo => (
-        <ItemRepo repo={repo} />
+        <ItemRepo repo={repo} handleRemoveRepo={handleRemoveRepo} />
       ))}
     </Container>
   );
