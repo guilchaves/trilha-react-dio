57bf02353802fb6fb23541f9ad1675e85f9892b1
guilherme <guilgchaves@protonmail.com>
2023-03-16 12:48:46 -0300

    fix button title

--- 04-login-form/src/components/Button/index.tsx
+++ 04-login-form/src/components/Button/index.tsx
@@ -2,7 +2,11 @@ import { ButtonContainer } from './styles';
 import { IButtonProps } from './types';
 
 const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick} title={title}></ButtonContainer>;
+  return (
+    <ButtonContainer onClick={onClick} title={title}>
+      {title}
+    </ButtonContainer>
+  );
 };
 
 export default Button;
