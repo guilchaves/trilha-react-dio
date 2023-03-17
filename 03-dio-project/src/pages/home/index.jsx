f72fce01c6bf3d21ec83304de4c94bb99c9a0e60
guilherme <guilgchaves@protonmail.com>
2023-03-17 13:39:06 -0300

    fix importing

--- 03-dio-project/src/pages/home/index.jsx
+++ 03-dio-project/src/pages/home/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '../../components';
 
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <Button>Teste 1</Button>
+      <Button variant="secondary">Teste 2</Button>
       <Link to="/login">Fazer login</Link>
     </>
   );
